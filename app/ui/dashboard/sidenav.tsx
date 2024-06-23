f9e25e4b81b3fff02bebf7c3d318910c6d1c66e7
dngvbngc <dvbngoc.0706@gmail.com>
2024-06-23 21:52:21 +0800

    fix side nav display

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
-import { PowerIcon } from "@heroicons/react/24/outline";
-import { signOut } from "@/auth";
 import CarpoolLogo from "@/app/ui/carpool-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
 
 export default function SideNav() {
   return (
@@ -15,23 +14,15 @@ export default function SideNav() {
           <CarpoolLogo />
         </div>
       </Link>
-      <div className='px-4 ml-[-10px] md:ml-0 fixed bottom-0 w-full h-20 md:h-auto bg-white md:relative md:flex md:bottom-auto'>
-        <div className='z-1 h-full md:h-auto flex grow flex-row items-center justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
-          <NavLinks />
-          <div className='hidden h-auto w-full grow rounded-md bg-gray-50 md:block'></div>
-          <form
-            className='md:w-full'
-            action={async () => {
-              "use server";
-              await signOut();
-            }}
-          >
-            <button className='hidden md:w-full md:flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>
-              <PowerIcon className='w-6' />
-              <div className='hidden md:block'>Sign Out</div>
-            </button>
-          </form>
-        </div>
+      <div className='flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
+        <NavLinks />
+        <div className='hidden h-auto w-full grow rounded-md bg-gray-50 md:block'></div>
+        <form>
+          <button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>
+            <PowerIcon className='w-6' />
+            <div className='hidden md:block'>Sign Out</div>
+          </button>
+        </form>
       </div>
     </div>
   );
